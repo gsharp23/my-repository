9221385aed4f80d6b348f038d0527c693f5fa05f
gsharp23 <117049936+gsharp23@users.noreply.github.com>
2023-01-23 20:38:36 -0500

    changes to code

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -9,7 +9,7 @@ def convert(decimal_num):
         num_to_roman += roman[i]*(decimal_num//i)
         decimal_num %= i
     return num_to_roman
-    
+
 @app.route('/', methods=['POST', 'GET'])
 def main_post():
     if request.method == 'POST':
@@ -27,5 +27,5 @@ def main_post():
         return render_template('index.html', developer_name='Guy Sharp', not_valid=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
