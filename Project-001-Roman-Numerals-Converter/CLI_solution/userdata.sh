ac84138192b0395924d5b2066435c3f55ac3f72b
gsharp23 <117049936+gsharp23@users.noreply.github.com>
2023-01-23 21:52:29 -0500

    update3

--- Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -1,10 +1,12 @@
-#! /bin/bash
 yum update -y
 yum install python3 -y
 pip3 install flask
-yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+FOLDER="https://github.com/gsharp23/my-repository/tree/main/Project-001-Roman-Numerals-Converter"
+wget ${FOLDER}/app.py
+mkdir templates
+cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
+cd ..
 python3 app.py
